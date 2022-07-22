e0ba2e0ba4287139c6316f49de39945f5b6b25d9
theopappas17 <theopappas17@gmail.com>
2022-07-21 21:24:37 -0400

    Do a dance

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,24 @@
 const apiKey = "03dfa929ee4dd9dfb9feba4929296d27";
 const submit = document.getElementById("submit");
 const destinationCity = document.getElementById("cityName");
+const displayWeather = document.getElementById("weather");
 
-function retrieveWeather(e) {
-  e.preventDefault();
+function retrieveWeather() {
   let destination = destinationCity.value; 
   let getWeather =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     destination +
-    "&appid=" +
+    "&exclude=minutely&appid=" +
     apiKey;
   getWeatherUrl = getWeather.replace(/\s/g, '&').trim();
   console.log(getWeatherUrl)
-  fetch(getWeatherUrl).then(function(response) {
-    return response.json();
-    })
-}
+  fetch(getWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+    displayWeather.innerHTML = data.weather[0].description;
+    });
+  }
 
 
-submit.addEventListener("click", retrieveWeather)
+submit.addEventListener("click", retrieveWeather);
